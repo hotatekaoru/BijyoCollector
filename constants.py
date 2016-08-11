a5d9d0b0f761cf29aa874b51f2c1d3e27fad3c84
Hotate <kaorupochi@gmail.com>
2016-08-11 15:47:03 +0900

    add classify images

--- constants.py
+++ constants.py
@@ -2,13 +2,14 @@
 IMAGES_NUM = 5
 
 # 判定時の画像サイズ（増やすと精度は上がるが遅くなる）
-CLASSIFY_IMG_SIZE_PX = 56
+CLASSIFY_IMG_SIZE_PX = 112
 
 ##################
 # パス
 ##################
 
 # 取得画像
-PATH_RET = 'static/img/retrieve/'
+PATH_RET =  'static/img/retrieve/'
 PATH_RECT = 'static/img/rect/'
 PATH_TRIM = 'static/img/trim/'
+PATH_TRAIN = 'static/img/train/'
